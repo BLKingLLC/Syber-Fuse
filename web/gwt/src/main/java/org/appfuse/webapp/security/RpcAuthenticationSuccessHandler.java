e802df1b2241dce457f046e60c7ea433fe29af30
bobby.king@blking.net <bobby.king@blking.net>
2017-03-09 17:06:44 -0500

    upgraded to spring 4.3x

--- web/gwt/src/main/java/org/appfuse/webapp/security/RpcAuthenticationSuccessHandler.java
+++ web/gwt/src/main/java/org/appfuse/webapp/security/RpcAuthenticationSuccessHandler.java
@@ -11,8 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.util.ELRequestMatcher;
-import org.springframework.security.web.util.RequestMatcher;
+import org.springframework.security.web.util.matcher.ELRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 /**
  * @author ivangsa
